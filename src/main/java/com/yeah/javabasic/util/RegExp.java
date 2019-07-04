bb8332889cdb01a32fb762993b34992128f40f11
zouye <zouye@techsure.com.cn>
2019-07-04 10:58:59 +0800

    Java的变参方法，正则表达式的Matcher appendReplacement方法

--- src/main/java/com/yeah/javabasic/util/RegExp.java
+++ src/main/java/com/yeah/javabasic/util/RegExp.java
@@ -1,12 +1,56 @@
 package com.yeah.javabasic.util;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegExp {
 	public static void main(String[] args) {
+		// 竖线在正则中有特殊含义，需转义
 		String sep = "\\|";
 		String envStr = "|SIT|PRD|";
 		String[] envArr = envStr.split(sep);
 		System.out.println(Arrays.asList(envArr));
+
+		// 测试使用 Matcher 的 appendReplacement 方法，
+		// $1~$9 有特殊含义
+		String originStr = "repotype.develop=git\r\n" + 
+				"repo.develop=http://192.168.0.22/deploy/branches/{{version}}\r\n" + 
+				"gitbranch.develop=develop\r\n" + 
+				"\r\n" + 
+				"repotype=git\r\n" + 
+				"repo=http://{{user}}:{{password}}@192.168.0.88/deploy/easydeploy.git\r\n" + 
+				"git.masterBranch=master\r\n" + 
+				"git.user=test\r\n" + 
+				"git.password= test \r\n" + 
+				"gitbranch=master\r\n" + 
+				"\r\n" + 
+				"agenttype=agentless\r\n" + 
+				"ostype=unix\r\n" + 
+				"osport=22\r\n" + 
+				"osuser=ADMIN\r\n" + 
+				"ospwd=aaaaaaa\r\n" + 
+				"\r\n" + 
+				"mailto=123456@techsure.com.cn,2222@sina.com\r\n" + 
+				"mailcc=\r\n" + 
+				"\r\n" + 
+				"log.patterns=/app/logs/balantflow/*\r\n" + 
+				"\r\n" + 
+				"instance.looptype=outer\r\n" + 
+				"instance.trigger.round=first\r\n" + 
+				"\r\n" + 
+				"";
+		Pattern pattern = Pattern.compile("(ospwd|password\\s*=)(.+)");
+		Matcher m = pattern.matcher(originStr);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			System.out.println(m.group());
+			System.out.println(m.group(2));
+			
+			m.appendReplacement(sb, "$1" + m.group(2).toUpperCase());
+		}
+		m.appendTail(sb);
+
+		System.out.println(sb.toString());
 	}
 }
---
+++ src/main/java/com/yeah/javabasic/util/Varargs.java
@@ -0,0 +1,30 @@
+package com.yeah.javabasic.util;
+
+/**
+ * 变参方法可接收一个数组参数
+ * @ClassName:  Varargs   
+ * @Description:TODO
+ * @author: zouye
+ * @date:   2019年7月4日 上午10:57:08   
+ *
+ */
+public class Varargs {
+	public  static void main(String[] args) {
+		int[] arr = {1,2,3,4,5,6};
+		System.out.println("给变长参数方法传入数组：");
+		test(arr);
+		System.out.println("给变长参数方法传入不定长参数：");
+		test(9, 6, 7, 8);
+	}
+	
+	// 定义变长参数的方法
+	 private static void test(int... params) {
+		 StringBuffer sb = new StringBuffer();
+		 for (int i = 0; i < params.length; i++) {
+			 sb.append(params[i]);
+			 sb.append(", ");
+		 }
+		 
+		System.out.println(sb.toString());
+	}
+}
